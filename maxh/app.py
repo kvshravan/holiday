e8945a908ac9fe6fbb0474be8ee2b68ac1389621
Kandadi <kandadishravan@gmail.com>
2022-04-01 21:49:55 +0530

    Ramp up the UI

--- maxh/app.py
+++ maxh/app.py
@@ -12,6 +12,44 @@ app = Flask(__name__)
 # To add other resources, create functions that generate the page contents
 # and add decorators to define the appropriate resource locators for them.
 
+class HighlightedCalendar(calendar.HTMLCalendar):
+    def __init__(self, highlight=[], *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._highlight = highlight
+    
+    def formatday(self, day, weekday):
+        """
+        Return a day as a table cell.
+        """
+        if day in self._highlight:
+            return '<td class="%s" style="font-weight: 750;color:red;" bgcolor="pink">%d</td>' % (self.cssclasses[weekday], day)
+        else:
+            if day == 0:
+            # day outside month
+                return '<td class="%s" style="font-weight: 750;">&nbsp;</td>' % self.cssclass_noday
+            else:
+                return '<td class="%s" style="font-weight: 750;">%d</td>' % (self.cssclasses[weekday], day)
+    
+    def formatmonth(self, theyear, themonth, withyear=True):
+        """
+        Return a formatted month as a table.
+        """
+        v = []
+        a = v.append
+        a('<table border="0" cellpadding="2" cellspacing="0" class="%s">' % (
+            self.cssclass_month))
+        a('\n')
+        a(self.formatmonthname(theyear, themonth, withyear=withyear))
+        a('\n')
+        a(self.formatweekheader())
+        a('\n')
+        for week in self.monthdays2calendar(theyear, themonth):
+            a(self.formatweek(week))
+            a('\n')
+        a('</table>')
+        a('\n')
+        return ''.join(v)
+            
 
 @app.after_request
 def add_header(response):
@@ -35,62 +73,76 @@ def isholi(day):
 
 
 def bestTimeInAMonth(yy, mm, k, s):
-    i = j = date(yy, mm, 1)
-    ans = 0
-    k = min(k,366)
-    s += "<br> Here is the best pick for you with atmost " + \
-        str(k) + " leaves in the month of " + \
-        calendar.month_name[mm]+":) <br><br>"
+    today = date.today()
+    if today.month == mm:
+        i = j = today
+    else:
+        i = j = date(yy, mm, 1)
+
+    k = min(k, 366)
+
     storage = [[0 for i in range(1, 33)] for j in range(1, 14)]
+
     maxj = maxi = date(yy, mm, 1)
-    while i.month <= mm:
+
+    while i.month <= mm and i.year <= yy:
         storage[j.month][j.day] = isholi(j)
         if not storage[j.month][j.day]:
             k -= 1
         if k < 0:
-            if (j-i).days > (maxj-maxi).days:
+            if (j - i).days > (maxj - maxi).days:
                 maxi = i
                 maxj = j
             while i.month <= mm and i <= j and storage[i.month][i.day]:
                 i += timedelta(days=1)
             k += 1
             i += timedelta(days=1)
-        if (j-i).days > (maxj-maxi).days:
+        if (j - i).days > (maxj - maxi).days:
             maxi = i
             maxj = j
         j += timedelta(days=1)
-    if (j-i).days > (maxj-maxi).days:
+
+    if (j - i).days > (maxj - maxi).days:
         maxi = i
         maxj = j
-    ans = (maxj-maxi).days
-    maxmonth = maxj.month+1
+
+    maxmonth = maxj.month + 1
+
     holidays = request.cookies.get('holidays')
     if holidays is None:
         holidays = allHolidays
     else:
         holidays = json.loads(holidays)
-    if maxj.year > maxi.year:
-        maxmonth = 13
-    for i in range(maxi.month, maxmonth):
-        s += (calendar.HTMLCalendar().formatmonth(maxi.year, i))
-        s += "<br>"
-    s += "<br>Maximum holidays - <b>"+str(ans) + "</b> <br>"
+
+    maxholidays = []
+    highlights_list = [[] for i in range(13)]
+
+    save_maxi_month = maxi.month
     while maxi < maxj:
         day = maxi
         d = day.strftime("%d %B, %Y")
-        s += "<br>"
+        highlights_list[maxi.month].append(maxi.day)
         var = storage[maxi.month][maxi.day]
         if not var:
-            s = s + "<b>" + d + " - Leave</b>"
+            maxholidays.append((d, 0))
+        elif var == 1:
+            maxholidays.append((d, 1))
         else:
-            if var == 1:
-                s = s + d + " - Holiday (Weekend)"
-            else:
-                s = s + d + " - Holiday" + \
-                            "(" + holidays[str(day)] + ")"
+            maxholidays.append((d, holidays[str(day)]))
         maxi += timedelta(days=1)
-    s += "<br>-----------------------------------------------------------------------------<br>"
-    return s
+
+    if maxj.year > maxi.year:
+        maxmonth = 13
+
+    calendars = []
+
+    for i in range(save_maxi_month, maxmonth):
+        c = HighlightedCalendar(highlight=highlights_list[i]).formatmonth(maxi.year, i)
+        calendars.append(c)
+    
+    print(calendars)
+
+    return maxholidays,calendars
 
 
 def topChoices(heap, storage, s):
@@ -105,8 +157,8 @@ def topChoices(heap, storage, s):
         h *= -1
         s += "<br>Maximum holidays - <b>" + str(h) + "</b> <br>"
         s += "<h4>from " + maxi.strftime("%d %B, %Y")
-        s += " to " + maxj.strftime("%d %B, %Y")+"<br></h4>"
-        maxmonth = maxj.month+1
+        s += " to " + maxj.strftime("%d %B, %Y") + "<br></h4>"
+        maxmonth = maxj.month + 1
         if maxj.year > maxi.year:
             maxmonth = 13
         for i in range(maxi.month, maxmonth):
@@ -132,7 +184,7 @@ def topChoices(heap, storage, s):
 
 def bestTimeInYear(yy, k, s):
     stack = []
-    k = min(k,366)
+    k = min(k, 366)
     s += "<br> Here are the best 20 choices picked for you with atmost " + \
         str(k) + " leaves starting from tomorrow :) <br>"
     storage = [[0 for i in range(1, 33)] for j in range(1, 14)]
@@ -147,30 +199,38 @@ def bestTimeInYear(yy, k, s):
         if not storage[j.month][j.day]:
             k -= 1
         if k < 0:
-            heapq.heappush(stack, (-(j-i).days, i, j))
+            heapq.heappush(stack, (-(j - i).days, i, j))
             while i <= j and storage[i.month][i.day]:
                 i += timedelta(days=1)
             k += 1
             i += timedelta(days=1)
-        heapq.heappush(stack, (-(j-i).days, i, j))
+        heapq.heappush(stack, (-(j - i).days, i, j))
         j += timedelta(days=1)
-    heapq.heappush(stack, (-(j-i).days, i, j))
+    heapq.heappush(stack, (-(j - i).days, i, j))
     return topChoices(stack, storage, s)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     # Render the page
+    highlight = range(1, 7)
     if request.method == "POST":
         yy = int(request.form.get('yy'))
         mm = int(request.form.get('mm'))
         k = int(request.form.get('k'))
-        s = '<meta name="viewport" content="width=device-width, initial-scale=1.0">'
+        s = ''
         if 'mmm' in request.form:
-            s = bestTimeInAMonth(yy, mm, k, s)
+            maxholidays,calendars = bestTimeInAMonth(yy, mm, k, s)
         else:
             s = bestTimeInYear(yy, k, s)
-        return s
+        return render_template(
+            'view.html',
+            noHolidays=len(maxholidays),
+            k=k,
+            month=calendar.month_name[mm],
+            holidays=maxholidays,
+            calendars=calendars,
+        )
     resp = make_response(render_template('index.html'))
     if 'holidays' not in request.cookies:
         resp.set_cookie('holidays', json.dumps(allHolidays))
@@ -211,9 +271,11 @@ def removeHoliday(key):
         print(e)
     return redirect(url_for('holiday'))
 
+
 @app.route('/about', methods=['GET'])
 def about():
     return render_template('about.html')
+
 
 allHolidays = {
     "2022-01-13": "Uruka /Lohri",
@@ -227,13 +289,13 @@ allHolidays = {
     "2022-07-10": "Id-ul-Zuha",
     "2022-08-09": "Muharram",
     "2022-08-15": "Independence Day",
-    "2022-10-02": "Mahatma Gandhi’s Birthday",
-    "2022-10-03": "Durga Puja ",
-    "2022-10-04": "Durga Puja (Navami)",
+    "2022-10-02": "Mahatma Gandhi's Birthday",
+    "2022-10-03": "Durga Puja",
+    "2022-10-04": "Navami",
     "2022-10-05": "Dussehra",
     "2022-10-09": "Id-e-Milad",
     "2022-10-24": "Deepavali",
-    "2022-11-08": "Guru Nanak’s Birthday",
+    "2022-11-08": "Guru Nanak's Birthday",
     "2022-12-25": "Christmas",
     "All-Saturdays": " ",
     "All-Sundays": " "
@@ -241,4 +303,4 @@ allHolidays = {
 
 if __name__ == '__main__':
     # Run the app server on localhost:5000
-    app.run()
+    app.run(debug=True)
